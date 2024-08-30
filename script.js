3c902f7b9152ad85e84348a9584d85a4b783add0
Hafizzargar <hafezzargar987@gmail.com>
2024-08-30 19:46:55 +0530

    bw

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+let codes = document.querySelectorAll(".code")
+
+codes.forEach((code,index)=>{
+    code.addEventListener("keydown",(event)=>{
+        if(event.key >= "0" && event.key <= "9"){
+            event.preventDefault();
+            code.value = event.key
+            if(codes[index+1]) {
+                codes.forEach(code => code.classList.remove("focused"));
+                setTimeout(() => codes[index + 1]?.focus(), 10);
+                codes[index+1].classList.add("focused")
+            }
+        } else if(event.key == "Backspace"){
+            code.value = ""
+            if(codes[index-1]) {
+                codes.forEach(code => code.classList.remove("focused"));
+                setTimeout(() => codes[index - 1]?.focus(), 10);
+                codes[index-1].classList.add("focused")
+            }
+        }
+    })
+})
